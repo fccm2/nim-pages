8b5d6ec79e0a213cadf272d47062c4e2db509813
Florent Monnier <monnier.florent@gmail.com>
2025-04-26 09:12:49 +0200

    add a minimal example with .getBoundingClientRect(), (test7)

---
+++ examples/test7.nim
@@ -0,0 +1,30 @@
+import pages
+
+type
+  Pos = object
+    x, y: cint
+
+type
+  Circles = object
+    list: seq[Pos]
+
+let cnv = getElementById("c1")
+let ctx = cnv.getContext2d()
+
+var circles = Circles(list: @[])
+
+proc draw_c(x: cint, y: cint) =
+  ctx.beginPath()
+  ctx.fillStyle(240, 180, 20)
+  ctx.circle(x, y, 22)
+  ctx.fill()
+
+proc mouse_event(m: MouseEvent) =
+  let rect = getCanvasBoundingRect(cnv)
+  let x = getClientX(m) - getLeft(rect)
+  let y = getClientY(m) - getTop(rect)
+  let p = Pos(x: x, y: y)
+  circles.list.insert(p, 0)
+  draw_c(x, y)
+
+addMouseDownEventListener(mouse_event)
